db9694590ab19fb5942ce951c113c19ba0d8a178
stephensalah <stephen@salahfamily.org>
2022-03-07 13:27:41 -0600

    Level 1 and mechanics started

---
+++ Wheres the Ground/Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    [SerializeField]private float speed;
+    
+    private bool grounded;
+    private void Awake(){
+        body = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update(){
+        body.velocity = new Vector2(Input.GetAxis("Horizontal")* speed,body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space)&& grounded){
+            Jump();
+            
+        }
+    }
+    private void Jump(){
+        body.velocity = new Vector2(body.velocity.x,speed);
+        grounded=false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag=="Ground"){
+            grounded=true;
+        }
+    }
+}
